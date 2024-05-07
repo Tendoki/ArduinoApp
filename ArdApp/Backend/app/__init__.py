a6e7fef5628ba844b5fab43af5e3b96ece431386
Tendoki <spore.mr.koshelev@gmail.com>
2024-05-07 13:29:00 +0300

    updated

--- ArdApp/Backend/app/__init__.py
+++ ArdApp/Backend/app/__init__.py
@@ -11,15 +11,12 @@ manager = LoginManager()
 def create_app():
     app = Flask(__name__)
 
-    url = "postgresql://postgres:root@localhost:5432/ArdAppDB"
+    app.config.from_pyfile('settings.py')
+    url = app.config['SQLALCHEMY_DATABASE_URI']
     # url = "postgresql://postgres:root@db:5432/ArdAppDB"
     engine = create_engine(url)
     if not database_exists(url):
         create_database(url)
-
-    app.config.from_pyfile('settings.py')
-    # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
-    # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
     manager.init_app(app)
